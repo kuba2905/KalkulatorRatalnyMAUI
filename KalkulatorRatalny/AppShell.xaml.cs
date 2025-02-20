c3e665e5197ecc3f4fda947dba76fcef78b62cff
jakub.matras <jakub.matras@S50-18>
2025-02-20 08:36:07 +0100

    Dodaj pliki projektów.

---
+++ KalkulatorRatalny/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace KalkulatorRatalny
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ KalkulatorRatalny/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace KalkulatorRatalny
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void OnLoginClicked(object sender, EventArgs e)
+        {
+            // Pobranie danych z pól wejściowych
+            string name = nameEntry.Text;
+            string email = emailEntry.Text;
+        }
+    }
+
+}
