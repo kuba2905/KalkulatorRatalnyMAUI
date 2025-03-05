669d5286424677f1e908394e956c8d706a815569
Jakub Golonka <jakubgolonka216@gmail.com>
2025-03-05 22:02:23 +0100

    Update MainPage.xaml.cs

--- KalkulatorRatalny/MainPage.xaml.cs
+++ KalkulatorRatalny/MainPage.xaml.cs
@@ -2,18 +2,17 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
         }
-        private void OnLoginClicked(object sender, EventArgs e)
+        
+        private void OnNadplataToggled(object sender, ToggledEventArgs e)
         {
-            // Pobranie danych z pól wejściowych
-            string name = nameEntry.Text;
-            string email = emailEntry.Text;
+            bool isSwitchOn = e.Value;
+
+            nadplataKredytuLabel.IsVisible = isSwitchOn;
+            nadplataKredytuInput.IsVisible = isSwitchOn;
         }
     }
-
 }
